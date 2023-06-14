902b99566d4a3c7ceda3c25c51020050bb0ac497
Krzychu <krzysiu.krzys65@gmail.com>
2023-06-14 15:06:34 +0200

    update

--- header.php
+++ header.php
@@ -1,37 +1,27 @@
 <header>
 
-<div class='statusLogowania'>
-<?php
-include_once("laczenieZbaza.php");
-if( isset( $_SESSION['email'] ) && !empty( $_SESSION['email'] ) ){
-    
-    //wyswietlanie email lub nicku użytkownika
-    if(!empty($_SESSION['nickname'])){
-        echo '<span>Zalogowano jako '. $_SESSION['nickname'] ."</span>";
-    }
-    else{
-        echo '<span>Zalogowano jako '. $_SESSION['email'] ."</span>";
-    }
-}
-?>
+<div class='menu1'>
+    <a href="index.php"><img src="svg/logo1.svg" alt="logo" width="100px" height="auto"></a>
+
 </div>
 
-<div class="menu">
-    <a href="index.php"><img src="svg/logo1.svg" alt="logo" width="110px" height="auto"></a>
-    <a href="index.php" class="linki">Strona główna</a>
+<div class="menu2">
+    <input type="text" placeholder="Wyszukaj..." >
 <?php
     if( !isset( $_SESSION['email'] ) && empty( $_SESSION['email'] ) ){
         echo "<a href='logowanie.php' class='linki'>Zaloguj</a>";
-    }
-
-    if( isset( $_SESSION['email'] ) && !empty( $_SESSION['email'] ) ){
-        echo "<a href='konto.php' class='linki'>Twoje konto</a>";
     }
 ?> 
 
 </div>
 <div class='wyloguj'>
 <?php
+
+
+    if( isset( $_SESSION['email'] ) && !empty( $_SESSION['email'] ) ){
+        echo "<a href='konto.php'><img src='svg/account.svg' width='30px' height='30px' alt='konto'></a>";
+    }
+
     if( isset( $_SESSION['email'] ) && !empty( $_SESSION['email'] ) ){
         echo "<form  action='wyloguj.php'  method='POST'>
                 <button id='wyloguj'><img src='svg/logout.svg' width='30px' height='30px' alt='&#9032'></button> </form>";
