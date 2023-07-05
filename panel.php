07c58aed8e4758437327e5149b511d05f8beb417
Krzychu <krzysiu.krzys65@gmail.com>
2023-07-05 14:35:59 +0200

    .

--- panel.php
+++ panel.php
@@ -12,7 +12,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="s.css?v=1.2">
+    <link rel="stylesheet" href="s.css?v=1.1">
     <title>Sesja</title>
 </head>
 <body>
