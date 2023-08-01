61523ecad3b7ca1ab588953d605ae6055d0e7c00
Krzychu <krzysiu.krzys65@gmail.com>
2023-08-01 15:50:24 +0200

    work

--- dodaj_do_koszyka.php
+++ dodaj_do_koszyka.php
@@ -3,30 +3,26 @@ session_start();
 $indeks = $_POST['indeks'];
 $rozmiar = $_POST['rozmiar'];
 $limit = $_POST['ilosc_rozmiaru'];
-$_SESSION['indeks_produktu'] = $indeks;
-$_SESSION['rozmiar_produktu'] = $rozmiar;
-$indeks = $_SESSION['indeks_produktu'];
-$rozmiar = $_SESSION['rozmiar_produktu'];
 $ilosc = 1;
 if(empty($_SESSION['koszyk'])){
     $koszyk= array($indeks, $rozmiar, $ilosc, $limit);
     $_SESSION['koszyk'] = array($koszyk);
 }
-elseif(!empty($_SESSION['koszyk'])){    
+else{    
     for($i=0; $i < sizeof($_SESSION['koszyk']); $i++){
-        if(in_array($indeks,$_SESSION['koszyk'][$i])){
-            $nowa_ilosc = $_SESSION['koszyk'][$i][2];
-            $nowa_ilosc += 1;
-            $_SESSION['koszyk'][$i][2] = $nowa_ilosc;
-        }
-        else{
-            $push_array = array($indeks, $rozmiar, $ilosc,$limit);
-            array_push($_SESSION['koszyk'], $push_array);
-            break;
-        }
+        echo in_array($indeks, $_SESSION['koszyk'][$i]);
 
     }
-
+    // if(){
+    //     $nowa_ilosc = $_SESSION['koszyk'][$i][2];
+    //     $nowa_ilosc += 1;
+    //     $_SESSION['koszyk'][$i][2] = $nowa_ilosc;
+    // }
+    // else{
+        // $push_array = array($indeks, $rozmiar, $ilosc, $limit);
+        // array_push($_SESSION['koszyk'], $push_array);
+    // }
 }
+
 print_r($_SESSION['koszyk']);
 ?>
--- wyloguj.php
+++ wyloguj.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 unset($_SESSION['email']);
+unset($_SESSION['expire']);
+unset($_SESSION['koszyk']);
+unset($_SESSION['dostep']);
 session_destroy();
 header("location: index.php");
 ?>
